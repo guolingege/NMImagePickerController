7a07ab10cf98db97751f617c63acc06440b26bf7
孙国林 <sunguolin@sunguolindeiMac.local>
2017-07-18 15:02:04 +0800

    Initial Commit

---
+++ AlbumCollectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlbumCollectionDemo
+//
+//  Created by 孙国林 on 2017/7/18.
+//  Copyright © 2017年 孙国林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlbumCollectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlbumCollectionDemo
+//
+//  Created by 孙国林 on 2017/7/18.
+//  Copyright © 2017年 孙国林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
